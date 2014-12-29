9142c61c6d79dacb5fe6db54242d6c2880b48514
xxyy <devnull@nowak-at.net>
2014-12-29 03:39:07 +0100

    Fix initial connections not being handled properly by Dynlist; #338

--- tools/src/main/java/io/github/xxyy/xlogin/bungee/dynlist/DynlistListener.java
+++ tools/src/main/java/io/github/xxyy/xlogin/bungee/dynlist/DynlistListener.java
@@ -4,7 +4,6 @@ import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -26,7 +25,7 @@ public class DynlistListener implements Listener {
 
     @EventHandler
     public void onServerSwitch(ServerConnectEvent evt) {
-        if(evt.getPlayer().hasPermission("xlogin.dynlist")) {
+        if (evt.getPlayer().hasPermission("xlogin.dynlist")) {
             return;
         }
 
@@ -35,56 +34,42 @@ public class DynlistListener implements Listener {
             for (DynlistEntry match : matches) {
                 if (!evt.getPlayer().hasPermission("xlogin.dlby." + match.getName())) {
                     evt.getPlayer().sendMessage(new ComponentBuilder("Du darfst diesen Server momentan nicht betreten!").color(ChatColor.RED).create());
-                    evt.setCancelled(true);
+                    sendToFallbackServer(evt);
+                    evt.setCancelled(true); //We reconnect so that we can watch for errors while connecting
                     return;
                 }
             }
         }
     }
 
-    @EventHandler
-    public void onPostLogin(final PostLoginEvent evt) {
-        if(evt.getPlayer().hasPermission("xlogin.dynlist")) {
-            return;
-        }
-
-        List<DynlistEntry> matches = manager.getMatches(evt.getPlayer().getServer().getInfo());
-        if (matches.isEmpty()) {
-            return;
-        }
-
-        for (DynlistEntry match : matches) {
-            if (evt.getPlayer().hasPermission("xlogin.dlby." + match.getName())) {
-                continue;
-            }
-
-            evt.getPlayer().sendMessage(new ComponentBuilder("Du darfst diesen Server momentan nicht betreten!").color(ChatColor.RED).create());
-            String fallbackServerName = evt.getPlayer().getPendingConnection().getListener().getFallbackServer();
-            if (evt.getPlayer().getServer().getInfo().getName().equalsIgnoreCase(fallbackServerName)) {
+    private boolean sendToFallbackServer(final ServerConnectEvent evt) {
+        String fallbackServerName = evt.getPlayer().getPendingConnection().getListener().getFallbackServer();
+        if (evt.getTarget().getName().equalsIgnoreCase(fallbackServerName)) {
+            if (evt.getPlayer().getServer() == null) { //Initial join
                 evt.getPlayer().disconnect(
                         new ComponentBuilder("MinoTopia wird gerade gewartet. Bitte versuche es später erneut. (1)").color(ChatColor.RED).create()
                 );
-                return;
             }
-            ServerInfo info = manager.getPlugin().getProxy().getServerInfo(fallbackServerName);
-            if(info == null) {
-                evt.getPlayer().disconnect(
-                        new ComponentBuilder("Unbekannter Ersatzserver. Bitte versuche es später erneut.").color(ChatColor.RED).create()
-                );
-                return;
-            }
-
-            evt.getPlayer().connect(info, new Callback<Boolean>() {
-                @Override
-                public void done(Boolean result, Throwable error) {
-                    if(!result || error != null) {
-                        evt.getPlayer().disconnect(
-                                new ComponentBuilder("MinoTopia wird gerade gewartet. Bitte versuche es später erneut. (2)").color(ChatColor.RED).create()
-                        );
-                    }
-                }
-            });
-            return;
+            return false;
         }
+        ServerInfo info = manager.getPlugin().getProxy().getServerInfo(fallbackServerName);
+        if (info == null) {
+            evt.getPlayer().disconnect(
+                    new ComponentBuilder("Unbekannter Ersatzserver. Bitte versuche es später erneut.").color(ChatColor.RED).create()
+            );
+            return false;
+        }
+
+        evt.getPlayer().connect(info, new Callback<Boolean>() {
+            @Override
+            public void done(Boolean result, Throwable error) {
+                if (!result || error != null) {
+                    evt.getPlayer().disconnect(
+                            new ComponentBuilder("MinoTopia wird gerade gewartet. Bitte versuche es später erneut. (2)").color(ChatColor.RED).create()
+                    );
+                }
+            }
+        });
+        return true;
     }
 }
