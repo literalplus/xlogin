320a17da68cdc631b67e2706b4a56939c046301f
xxyy <devnull@nowak-at.net>
2015-12-04 22:57:53 +0100

    Fix a NPE in /register with illegitimate players; Fixes #483
    Joinbots would send chat messages before being connected, which is impossible for legitimate players to do.
    (cherry picked from commit be3df5a)

--- bungee/src/main/java/io/github/xxyy/xlogin/bungee/command/CommandRegister.java
+++ bungee/src/main/java/io/github/xxyy/xlogin/bungee/command/CommandRegister.java
@@ -1,12 +1,14 @@
 package io.github.xxyy.xlogin.bungee.command;
 
-import io.github.xxyy.common.bungee.ChatHelper;
-import io.github.xxyy.xlogin.bungee.XLoginPlugin;
-import io.github.xxyy.xlogin.common.authedplayer.AuthedPlayer;
-import io.github.xxyy.xlogin.common.authedplayer.AuthedPlayerFactory;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import io.github.xxyy.common.bungee.ChatHelper;
+import io.github.xxyy.common.chat.XyComponentBuilder;
+import io.github.xxyy.xlogin.bungee.XLoginPlugin;
+import io.github.xxyy.xlogin.common.authedplayer.AuthedPlayer;
+import io.github.xxyy.xlogin.common.authedplayer.AuthedPlayerFactory;
 
 /**
  * Handles registering users.
@@ -24,33 +26,44 @@ public class CommandRegister extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if(ChatHelper.kickConsoleFromMethod(sender)){
+        if (ChatHelper.kickConsoleFromMethod(sender)){
             return;
         }
 
         ProxiedPlayer plr = (ProxiedPlayer) sender;
 
-        if(plugin.getRepository().isPlayerKnown(plr.getUniqueId())) {
+        if (plr.getServer() == null){
+            /*
+            Legitimate players can't chat before they're connected to a server.
+            Some joinbots do, however, not wait until the connection has been
+            established before sending /register. Since there is no way to do
+            this with a legit Vanilla client, we can kick them.
+             */
+            plr.disconnect(new XyComponentBuilder("Bist du ein Joinbot?").create());
+            return;
+        }
+
+        if (plugin.getRepository().isPlayerKnown(plr.getUniqueId())){
             plr.sendMessage(plugin.getMessages().parseMessageWithPrefix(plugin.getMessages().alreadyRegistered));
             return;
         }
 
-        if(args.length < 2 || args[0].equalsIgnoreCase("help")) {
+        if (args.length < 2 || args[0].equalsIgnoreCase("help")){
             plr.sendMessage(plugin.getMessages().parseMessageWithPrefix(plugin.getMessages().commandRegisterUsage));
             return;
         }
 
-        if(!args[0].equals(args[1])) {
+        if (!args[0].equals(args[1])){
             plr.sendMessage(plugin.getMessages().parseMessageWithPrefix(plugin.getMessages().passwordsDontMatch));
             return;
         }
 
-        if(args[0].length() < 5) {
+        if (args[0].length() < 5){
             plr.sendMessage(plugin.getMessages().parseMessageWithPrefix(plugin.getMessages().passwordTooShort));
             return;
         }
 
-        if(plr.getName().equalsIgnoreCase(args[0]) || plugin.getConfig().getUnsafePasswords().contains(args[0])) {
+        if (plr.getName().equalsIgnoreCase(args[0]) || plugin.getConfig().getUnsafePasswords().contains(args[0])){
             plr.sendMessage(plugin.getMessages().parseMessageWithPrefix(plugin.getMessages().passwordInsecure));
             return;
         }
