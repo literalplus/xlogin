9241d39caf1352c995256590000abccdc9713da2
Literallie <git@l1t.li>
2017-08-31 23:03:45 +0200

    Change license to AGPLv3

---
+++ README.md
@@ -0,0 +1,16 @@
+xLogin
+------
+An advanced authentication application and awesome punishment
+management thing.
+
+**Note:** While it still works for the most part, a huge pile
+of this project's code is considered legacy. Reading these
+parts of the source may cause permanent brain damage. The
+author may under no condition be held liable for such
+damage. Take care.
+
+License
+-------
+
+This project is licensed under a GNU AGPLv3 license.
+Details may be found in the `LICENSE` file.
--- common/src/main/java/li/l1t/xlogin/common/module/ModuleManager.java
+++ common/src/main/java/li/l1t/xlogin/common/module/ModuleManager.java
@@ -1,11 +1,19 @@
 /*
- * Copyright (C) 2014-2016 Philipp Nowak (Literallie; xxyy98+xlo@gmail.com; The Author)
+ * xLogin - An advanced authentication application and awesome punishment management thing
+ * Copyright (C) 2013 - 2017 Philipp Nowak (https://github.com/xxyy)
  *
- * This application and all related code, assets and concepts are protected by international Copyright laws.
- * Any usage, including, but not limited to, decompilation, execution, compilation and distribution,
- *  is explicitly and strictly prohibited without explicit written permission from The Author.
- * Any such permission can be revoked at any time.
- * Legal steps may be taken in case of a violation of these terms.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package li.l1t.xlogin.common.module;
